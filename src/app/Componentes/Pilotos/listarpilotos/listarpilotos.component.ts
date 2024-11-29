f4cb7f09a766894b1edb5793e158f4379f8b3520
TR Network <TR Network@DESKTOP-NT2PUE0>
2024-11-28 23:54:13 -0600

    pilotos lista

--- src/app/Componentes/Pilotos/listarpilotos/listarpilotos.component.ts
+++ src/app/Componentes/Pilotos/listarpilotos/listarpilotos.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PilotosService } from '../../../Service/pilotos.service';
 
 @Component({
   selector: 'app-listarpilotos',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './listarpilotos.component.html',
   styleUrl: './listarpilotos.component.css'
 })
 export class ListarpilotosComponent {
-
+  constructor(private service:PilotosService){
+    this.service.getPilotos();
+   }
+   get listaPilotos(){
+     return this.service.listapilotos;
+   }
+   
 }
--- src/app/Service/pilotos.service.ts
+++ src/app/Service/pilotos.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PilotosService {
-
-  constructor() { }
+  public listapilotos: any [] =[];
+  public piloto:any;
+  
+    constructor(private http:HttpClient) { 
+      this.listapilotos=[];
+    }
+    getPilotos(){
+      console.log("da");
+      this.http.get('http://localhost:5202/api/Pilotos').subscribe((data:any)=>{
+        console.log(data);
+        this.listapilotos=data;
+      });
+    }
+    getPiloto(id:number){
+      this.http.get('http://localhost:5202/api/Pilotos'+id).subscribe((data:any)=>{
+        console.log(data);
+        this.piloto=data;
+      });
+    }
+  
+  
 }
